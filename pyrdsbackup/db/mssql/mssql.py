f89cc1f3c26ab275197a0aecf2671272e93bba7b
Safal Raj Pandey <safal.pandey.sp@gmail.com>
2018-08-28 18:23:28 +0545

    Fix formatting and add doc comments

--- pyrdsbackup/db/mssql/mssql.py
+++ pyrdsbackup/db/mssql/mssql.py
@@ -1,58 +1,99 @@
 import os
+import time
 import yaml
 import pyodbc
-import time
 import datetime
 
+
 def generate_rds_backup_path(db):
-    return 'backups/{}/{}-{}.bak'.format(db, db, datetime.datetime.now().strftime("%I-%M-%B-%d-%Y"))
+    '''
+    Generate a name for rds backup.
+    '''
+    return 'backups/{}/{}-{}.bak'.format(db, db, datetime.datetime.now().strftime('%I-%M-%B-%d-%Y'))
+
 
 def generate_native_backup_path(db):
-    return '{}-{}.bak'.format(db, datetime.datetime.now().strftime("%I-%M-%B-%d-%Y"))
+    '''
+    Generate a name for ec2 db backup.
+    '''
+    return '{}-{}.bak'.format(db, datetime.datetime.now().strftime('%I-%M-%B-%d-%Y'))
+
 
 def initialize_cursor(server, username, password, port, version):
-    ODBC_DRIVER = '{ODBC Driver %d for SQL Server}' %version
+    '''
+    Initialize db cursor using given connection details.
+    '''
+    ODBC_DRIVER = '{ODBC Driver %d for SQL Server}' % version
     CONNECTION_STRING = 'DRIVER={driver};SERVER={server};PORT={port};DATABASE={database};UID={username};PWD={password}'
     connection_str = CONNECTION_STRING.format(
-            driver=ODBC_DRIVER,
-            server=server,
-            database='msdb',
-            username=username,
-            password=password,
-            port=port
-        )
+        driver=ODBC_DRIVER,
+        server=server,
+        database='msdb',
+        username=username,
+        password=password,
+        port=port
+    )
     conn = pyodbc.connect(connection_str)
     conn.autocommit = True
     return conn.cursor()
 
+
 def rds_backup_procedure_exists(cursor):
-    cursor.execute(""" SELECT * FROM sys.objects
-            WHERE object_id = OBJECT_ID(N'dbo.rds_backup_database'); """)
+    '''
+    Returns true if dbo.rds_backup_database procedure exists.
+    '''
+    cursor.execute('''
+        SELECT * FROM sys.objects
+        WHERE object_id = OBJECT_ID(N'dbo.rds_backup_database');
+    ''')
+
     if cursor.fetchall() != []:
-            return True
+        return True
     return False
 
+
 def start_rds_backup(db, s3_bucket, s3_backup_object_path, cursor):
-    cursor.execute(""" exec msdb.dbo.rds_backup_database 
-    @source_db_name={},     
-    @s3_arn_to_backup_to='arn:aws:s3:::{}/{}',
-    @overwrite_S3_backup_file=1,
-    @type='full'; 
-    """.format(db, s3_bucket, s3_backup_object_path))
+    '''
+    Trigger dbo.rds_backup_database procedure and return its task id.
+    '''
+    cursor.execute('''
+        EXEC msdb.dbo.rds_backup_database
+            @source_db_name={},
+            @s3_arn_to_backup_to='arn:aws:s3:::{}/{}',
+            @overwrite_S3_backup_file=1,
+            @type='full';
+    '''.format(db, s3_bucket, s3_backup_object_path))
     return cursor.fetchall()[0][0]
 
+
 def start_native_backup(db, backup_location, backup_path, cursor):
+    '''
+    Run native backup query and return its task id.
+    '''
     print('Backing up DB: {} \n'.format(db))
     try:
-            cursor.execute("BACKUP DATABASE {} TO DISK='{}/{}' ;".format(db, backup_location, backup_path))
+        cursor.execute('''
+            BACKUP DATABASE {} TO DISK='{}/{}';
+        '''.format(db, backup_location, backup_path))
+
     except Exception as e:
-            print('Oops!!! Something went wrong.\n', e)
+        print('Oops!!! Something went wrong.\n', e)
+
 
 def task_status(task_id, cursor):
-    cursor.execute(""" exec msdb.dbo.rds_task_status @task_id={}""".format(task_id))
+    '''
+    Returns rds task status of given task id.
+    '''
+    cursor.execute('''
+        EXEC msdb.dbo.rds_task_status @task_id={}
+    '''.format(task_id))
     return cursor.fetchall()
 
+
 def task_completed(task_id, cursor):
+    '''
+    Returns true if a task is complete and raises an exception if it fails.
+    '''
     status = task_status(task_id, cursor)
     if status[0][5] == 'ERROR':
         print('Error')
